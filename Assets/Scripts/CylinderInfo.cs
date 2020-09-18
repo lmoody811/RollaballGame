5b807a7d4d4528e37e1de1e73ff057a09866126d
Stanley Morales <abrahamx.carranza@gmail.com>
2020-09-17 22:15:44 -0500

    Moving objects in level 3, changing scenario for level 3

---
+++ Assets/Scripts/CylinderInfo.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using UnityEngine;
+
+public class CylinderInfo : MonoBehaviour
+{
+    public float speed;
+    public float AngularSpeed;
+    protected Rigidbody r;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        r = GetComponent<Rigidbody>();    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        speed = r.velocity.magnitude;
+        AngularSpeed = r.angularVelocity.magnitude;
+
+        r.AddTorque(Vector3.forward);
+    }
+}
